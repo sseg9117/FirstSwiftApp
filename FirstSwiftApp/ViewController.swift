c89ee10419012fee13b72e3e743090798d3a3b2c
Sheradon Segota <31513874+sseg9117@users.noreply.github.com>
2017-10-06 09:22:02 -0600

    made dope app.

--- FirstSwiftApp/ViewController.swift
+++ FirstSwiftApp/ViewController.swift
@@ -28,7 +28,20 @@ class ViewController: UIViewController
         {
             firstButton.backgroundColor = .green
         }
+        view.backgroundColor = createRandomColor()
     }
+    
+    private func createRandomColor() -> UIColor
+    {
+        let newColor :UIColor
+        let redAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let greenAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        let blueAmount = CGFloat (Double (arc4random_uniform(256))/255.00)
+        newColor = UIColor(red: redAmount, green: greenAmount, blue: blueAmount, alpha: CGFloat(1.0))
+        
+        return newColor
+    }
+    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
